9f55e49f5f06779f68e596ae98a4f4a23d81d210
Mark Tomko <mtomko@broadinstitute.org>
2024-01-05 08:08:48 -0800

    Even smaller repro case

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,15 @@
-import cats.implicits._
+import cats.syntax.all._
 
 object Main {
 
   final case class Packet(flag: Int)
 
-  val packets: List[Packet] = List()
-
-  lazy val either: Either[Unit, Unit] = {
+  def main(args: Array[String]): Unit = {
     Left(()).recoverWith { _ =>
-      packets.find(_.flag == 1) match {
+      List.empty[Packet].find(_.flag == 1) match {
         case None    => Left(())
         case Some(_) => Right(())
       }
-    }
-  }
-
-  def main(args: Array[String]): Unit = {
-    either match {
-      case Left(_)  => println("doh")
-      case Right(_) => println("yay")
     }
   }
 

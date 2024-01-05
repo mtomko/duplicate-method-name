8854559ee99a9fc944ee5eb081a625aa5bf72bdf
Mark Tomko <mtomko@broadinstitute.org>
2024-01-05 08:11:02 -0800

    Smaller still

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,11 +2,9 @@ import cats.syntax.all._
 
 object Main {
 
-  final case class Packet(flag: Int)
-
   def main(args: Array[String]): Unit = {
     Left(()).recoverWith { _ =>
-      List.empty[Packet].find(_.flag == 1) match {
+      List.empty[Int].find(_ == 1) match {
         case None    => Left(())
         case Some(_) => Right(())
       }
